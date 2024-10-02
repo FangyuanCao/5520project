da75d5004fc2ae4ea4d1034ac96cf0cfb9240b82
Weimin Zhao 100588860 <32662816+WeiminZhao@users.noreply.github.com>
2024-10-02 16:28:21 -0400

    template for backend
    Add basic functions for backend. The DB yet to be done, however, the current version can be used to test frontend functions

---
+++ coffee-shop-backend/app.py
@@ -0,0 +1,108 @@
+from functools import wraps
+import secrets
+from flask import Flask, jsonify, render_template
+from flask import request
+from flask_cors import CORS, cross_origin
+
+app = Flask(__name__)
+app.secret_key = "123"
+
+@app.route('/')
+def index():
+    return 'welcome'
+
+@app.route('/registration', methods = ["POST"])
+def handle_registration():
+    uid = request.json.get('user_name')
+    password = request.json.get('password')
+
+    print(f"received registration from {uid} with password {password}")
+
+    return jsonify({'status':'complete'})
+
+@app.route("/login", methods=["POST"])
+def handle_user_login():
+    uid = request.json.get('user_name')
+    password = request.json.get('password')
+
+    print(f"received login request from {uid} with password {password}")
+
+    # authentication process
+
+    ##########################
+
+    # if autherized
+
+    server_auth = secrets.token_urlsage(16)
+
+    return jsonify({'status':'complete','authentication':server_auth})
+
+def token_required(f):
+    @wraps(f)
+    def decorated(*args, **kwargs):
+        token = None
+
+        print(request.headers)
+        if 'Authorization' in request.headers:
+            token = request.headers['Authorization']
+        if not token:
+            return jsonify({'status':'no authorization token!'}), 403
+        
+        # verify with DB
+        uid = 'test'
+        #
+        return f(uid, *args, **kwargs)
+    return decorated
+
+
+@app.route("/fetch_products", methods=["POST"])
+def get_products():
+    type = request.json.get('type')
+    number = request.json.get('number')
+
+    print(f"received fetch product request with product type {type} with number {number}")
+
+    return jsonify({
+        "product_list":[
+            {"id":1, "name":"coffee","type": "beverage", "price":[1,2,3], "options":["S","M","L"], "status":True}, #type can be food or beverage or combo
+            {"id":2, "name":"Tea","type": "beverage", "price":[1], "options":["S"], "status":True},
+            {"id":3, "name":"cookie","type": "food", "price":[5], "options":["pack"], "status":False},
+            {"id":4, "name":"NukaCola","type": "beverage", "price":[1,2,3,5,6], "options":["S","M","L","EXL","ABSL"], "status":True},
+            {"id":5, "name":"This_shop","type": "special", "price":[999999], "options":["combo"], "status":True},
+        ]
+    })
+
+@app.route("/update_products", methods=["POST"])
+def get_products():
+
+    id = request.json.get('id')
+    name = request.json.get('name')
+    type = request.json.get('type')
+    price = request.json.get('price')
+    options = request.json.get('options')
+    status = request.json.get('status')
+
+
+    print(f"received to update a product with product id {id} name {name} type {type} price {str(price)} options {str(options)} status {str(status)}")
+
+
+    #perform update based on id
+
+    return jsonify({'status':'complete'})
+
+@app.route("/customer_purchasing", methods=["POST"])
+@token_required
+def purchase(uid):
+    subtotal = request.json.get('subtotal')    
+
+
+    print(f"customer {uid} requests to purchase for subtotal of {subtotal}")
+    #execute
+
+    return jsonify({'url':'test'})
+
+if __name__ == "__main__":
+
+    app.run(host='0.0.0.0',port=3000)
+
+    
