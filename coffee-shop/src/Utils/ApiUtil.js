f50f3bdca48e5d75b727bc1d73c26f0eeda17e44
FangyuanCao <55753314+FangyuanCao@users.noreply.github.com>
2024-10-29 22:11:10 -0400

    create API utils create post and Http utils
    i create API and Http utils, now we can post and get method to sending and retrieving data with server

---
+++ coffee-shop/src/Utils/ApiUtil.js
@@ -0,0 +1,11 @@
+export default class ApiUtil{
+    static URL_IP = 'http://127.0.0.1:3000';
+    static URL_ROOT = '';
+
+    static API_REGISTRATION = ApiUtil.URL_ROOT + '/registration';
+    static API_LOGIN = ApiUtil.URL_ROOT + '/login';
+    static API_FETCH_PRODUCTS = ApiUtil.URL_ROOT + '/fetch_products';
+    static API_UPDATE_PRODUCTS = ApiUtil.URL_ROOT + '/update_products';
+    static API_CUSTOMER_PURCHASING = ApiUtil.URL_ROOT + '/customer_purchasing';
+    
+}
